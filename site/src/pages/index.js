c85a4f936dd9de5b86e86253b4ffe797c1e1b8b0
Shang-En Huang <math.tmt514@gmail.com>
2022-12-05 14:32:12 -0500

    Added chartjs

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -7,11 +7,58 @@ import {
   faHandLizard,
   faPlay,
   faForward,
+  faMagicWandSparkles,
+  faPause,
 } from '@fortawesome/free-solid-svg-icons'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+export const options = {
+  responsive: true,
+  animation: {
+    duration: 1,
+  },
+  scales: {
+    y: {
+      min: 0.0,
+      max: 1.0,
+    },
+  },
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
 
 const links = [
   {
@@ -137,12 +184,16 @@ class Simulator extends React.Component {
   static GetDefaultState() {
     var state = {
       cmdQueue: [],
+      coinSeq: 0,
       currentCoinIndex: -1,
       currentScriptInnerStep: 0,
       tossed: 0,
       heads: 0,
       tails: 0,
       message: "",
+      history: [],
+      historyHeadsProb: [],
+      historyCoinSeq: [],
       hasError: false
     };
     return {...state};
@@ -183,8 +234,14 @@ class Simulator extends React.Component {
       var p = this.props.urn[state.currentCoinIndex];
       if (Math.random() <= p) {
         state.heads += 1;
+        state.history.push(1);
+        state.historyHeadsProb.push(state.heads / state.tossed);
+        state.historyCoinSeq.push(state.coinSeq);
       } else {
         state.tails += 1;
+        state.history.push(0);
+        state.historyHeadsProb.push(state.heads / state.tossed);
+        state.historyCoinSeq.push(state.coinSeq);
       }
 
       // update cmdQueue
@@ -201,6 +258,7 @@ class Simulator extends React.Component {
       this.setState(state, callback);
     } else if (cmd[0] === "DrawAnotherCoin") {
       state.message = "OK draw another coin."
+      
 
       var newCoin = Math.floor(Math.random() * this.props.urn.length);
       var cnt=0;
@@ -211,6 +269,7 @@ class Simulator extends React.Component {
         console.log(newCoin, this.props.urn);
       }
 
+      state.coinSeq += 1;
       state.currentCoinIndex = newCoin;
       state.cmdQueue.shift();
       this.setState(state, callback);
@@ -260,10 +319,57 @@ class Simulator extends React.Component {
     this.setupTossByCount(count);
     return false
   }
+  holdOn() {
+    var state = {...this.state};
+    state.cmdQueue = [];
+    this.setState(state, this.theUltimateCallback.bind(this));
+  }
 
   render() {
     var vh = this.state.heads / this.state.tossed;
     var vt = this.state.tails / this.state.tossed;
+    var labels = [];
+    var totaldata = [];
+    var latestdata = [];
+    var currentTossed = 0;
+    var currentHead = 0;
+    for (var i = 1; i <= this.tosslimit; i++) {
+      labels.push(i);
+      if (i <= this.state.history.length) {
+        totaldata.push(this.state.historyHeadsProb[i-1]);
+        if (this.state.historyCoinSeq[i-1] === this.state.coinSeq) {
+          currentTossed += 1;
+          currentHead += (this.state.history[i-1]);
+          latestdata.push(currentHead / currentTossed);
+        } else {
+          latestdata.push(null);
+        }
+        
+      } else {
+        totaldata.push(null);
+        latestdata.push(null);
+      }
+    }
+    var vc = currentHead / currentTossed;
+
+    var chartdata = {
+      labels,
+      datasets: [
+        {
+          label: 'Overall',
+          data: totaldata,
+          borderColor: 'rgb(53, 162, 135)',
+          backgroundColor: 'rgba(53, 162, 135, 0.5)',
+        },
+        {
+          label: 'Latest',
+          data: latestdata,
+          borderColor: 'rgb(235, 99, 132)',
+          backgroundColor: 'rgba(235, 99, 132, 0.5)',
+        }
+      ],
+    };
+
     return (
       <div>
         <h2>Operation Panel</h2>
@@ -280,12 +386,17 @@ class Simulator extends React.Component {
       <li>
       <button className="toss toss-count" onClick={this.toss.bind(this)}>
         <FontAwesomeIcon icon={faPlay} />
-      </button> Toss <input id="toss-count" defaultValue={500} /> Times.
+      </button> Toss <input id="toss-count" defaultValue={1} /> Time(s).
       </li>
       <li>
       <button className="toss toss-all" onClick={this.tossAll.bind(this)}>
         <FontAwesomeIcon icon={faForward} />
       </button> Toss All Remaining {this.tosslimit - this.state.tossed} Coins.
+      </li>
+      <li>
+      <button className="toss toss-all" onClick={this.holdOn.bind(this)}>
+        <FontAwesomeIcon icon={faPause} />
+      </button> Hold On
       </li>
         </ul>
 
@@ -299,7 +410,45 @@ class Simulator extends React.Component {
         Tossed Coins: {this.state.tossed}<br/>
         Heads: {this.state.heads} ({(vh*100.0).toFixed(2)}%)<br/>
         Tails: {this.state.tails} ({(vt*100.0).toFixed(2)}%)<br/>
+        <span style={{color: '#888'}}>Heads for Latest Coin: {currentHead}/{currentTossed} ({(vc*100.0).toFixed(2)}%)</span><br/>
+        <Line options={options} data={chartdata} />
         </div>
+    );
+  }
+}
+
+class UrnMessage extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  say(n) {
+    var p=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"];
+    if (n <= 12) {
+      return p[n-1];
+    } else {
+      return n;
+    }
+
+  }
+  render() {
+    var urn_message = "You are given an urn with two biased coins $p=0.7$ and $p=0.3$.";
+    var urn = this.props.urn;
+    console.log("urn here", urn);
+    if (urn.length === 2) {
+      urn_message = "You are given an urn with two biased coins $p=" + urn[0] + "$ and $p=" + urn[1] + "$. ";
+    } else if (urn.length > 2) {
+      var avg = 0.0;
+      for (var x of urn) avg += x;
+      avg = avg / urn.length;
+      avg = avg.toFixed(3);
+
+      urn_message = "You are given an urn with "
+      + this.say(urn.length) + " coins with an average head probability " + "$\\mathbf{E}[p]="+ avg + "$" + ". ";
+    }
+
+    var msg = urn_message + "You have drawn a coin from the urn, but you cannot tell which coin is which from its appearance. The goal is to toss as many heads as possible.";
+    return (
+    <p id="urn-message" dangerouslySetInnerHTML={{__html: msg}} />
     );
   }
 }
@@ -309,7 +458,7 @@ class WorkingArea extends React.Component {
   constructor(props) {
     super(props)
     var initstate = {
-      urn: [0.3, 0.7],
+      urn: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
       command: "Draw()\nToss(500)\nDrawAnotherCoin()\nToss()",
       scriptCommands: [],
       msg: "",
@@ -371,22 +520,40 @@ class WorkingArea extends React.Component {
     this.setState(state);
   }
 
-  runSimulation() {
+  redefineUrn() {
     if (window.allTimeOuts !== undefined) {
       for (var id of window.allTimeOuts) {
         window.clearTimeout(id);
       }
     }
     window.allTimeOuts = [];
-    
+
+    var val = document.getElementById("urn").value;
     var state = {...this.state};
+    state.urn = eval(val);
     state.trialSeq += 1;
-    state.allowToStart = true;
     this.setState(state);
   }
 
+  componentDidUpdate() {
+    window.MathJax.typeset();
+    window.MathJax.typesetClear();
+  }
+  
+
   render() {
+    
+    // if (document.getElementById('urn-message') !== null) {
+    //   document.getElementById('urn-message'). innerHTML="";
+    // }
     return (<div>
+      <h2>Define Your Urn</h2>
+      <p>
+        <input id="urn" defaultValue="[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]" />
+        <button className="toss" onClick={this.redefineUrn.bind(this)}><FontAwesomeIcon icon={faMagicWandSparkles}/></button>
+      </p>
+      <h2>Instructions</h2>
+      <UrnMessage urn={this.state.urn} />
       <Simulator seq={this.state.trialSeq} urn={this.state.urn} cmds={this.state.scriptCommands}></Simulator>
     </div>)
   }
@@ -396,16 +563,12 @@ class TossCoinMain extends React.Component {
   componentDidMount() {
     if (window.MathJax !== undefined) {
       window.MathJax.typeset();
+      window.MathJax.typesetClear();
     }
   }
   render() {
     return (<Layout>
       <h1>Coin Tossing Simulation</h1>
-      <h2>Instructions</h2>
-      <p>
-        You are given an urn with two biased coins $p=0.7$ and $p=0.3$.
-        You have drawn a coin from the urn, but you cannot tell which coin is which from its appearance.
-      </p>
       <WorkingArea></WorkingArea>
     </Layout>)
   }
